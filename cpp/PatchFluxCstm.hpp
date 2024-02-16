bd772abee403a004768703ed9928168183980337
maximilian-brodbeck <maximilian.brodbeck@isd.uni-stuttgart.de>
2024-02-16 07:23:17 +0100

    Add new base-class for custom patches
    * Derived from PatchNew
    * Constructs DOFmap for flux and stress equilibration in cell-wise
      manner
    
    Open issues:
    * Add required getter-methods
    * Derive PatchCstmFlux from PatchCstm and add DOFs of RHS and
      DG-Flux
    * Implement new patch-class into semi-explicit equilibration

--- cpp/PatchFluxCstm.hpp
+++ cpp/PatchFluxCstm.hpp
@@ -6,12 +6,487 @@
 #include <dolfinx/fem/FiniteElement.h>
 #include <dolfinx/fem/FunctionSpace.h>
 
+#include <algorithm>
 #include <cassert>
 
 using namespace dolfinx;
 
 namespace dolfinx_eqlb
 {
+template <typename T, int id_flux_order = 3, bool constr_minms = false>
+class PatchCstm : public PatchNew
+{
+public:
+  /// Initialization
+  ///
+  /// Storage is designed for the maximum patch size occurring within
+  /// the current mesh. (Cell-IDs start at 1, facet-IDs at 0!)
+  ///
+  /// @param nnodes_proc             Number of nodes on current processor
+  /// @param mesh                    The current mesh
+  /// @param bfct_type               List with type of all boundary facets
+  /// @param function_space_fluxhdiv Function space of H(div) flux
+  /// @param function_space_fluxdg   Function space of projected flux
+  /// @param basix_element_fluxdg    BasiX element of projected flux
+  ///                                (continuous version for required
+  ///                                entity_closure_dofs)
+  PatchCstm(
+      int nnodes_proc, std::shared_ptr<const mesh::Mesh> mesh,
+      mdspan_t<const std::int8_t, 2> bfct_type,
+      const std::shared_ptr<const fem::FunctionSpace> function_space_fluxhdiv)
+      : PatchNew(nnodes_proc, mesh, bfct_type),
+        _degree_elmt_fluxhdiv(
+            function_space_fluxhdiv->element()->basix_element().degree() - 1),
+        _function_space_fluxhdiv(function_space_fluxhdiv)
+  {
+    assert(id_flux_order < 0);
+
+    // Set DOF counters of flux element
+    _ndof_flux = _function_space_fluxhdiv->element()->space_dimension();
+
+    _ndof_flux_fct = _degree_elmt_fluxhdiv + 1;
+
+    _ndof_flux_cell = _ndof_flux - _fct_per_cell * _ndof_flux_fct;
+    _ndof_flux_add_cell
+        = 0.5 * _degree_elmt_fluxhdiv * (_degree_elmt_fluxhdiv - 1);
+    _ndof_flux_div_cell = _ndof_flux_cell - _ndof_flux_add_cell;
+
+    _ndof_flux_nz = _ndof_flux - (_fct_per_cell - 2) * _ndof_flux_fct;
+
+    // Resize storage of DOFmap
+    if constexpr (constr_minms)
+    {
+      const std::size_t ndofs_per_cell
+          = std::max(2 * _ndof_flux_fct + _ndof_flux_add_cell + _fct_per_cell,
+                     _ndof_flux_nz);
+      _ddofmap.resize((_ncells_max + 2) * ndofs_per_cell * 4, 0);
+      _dofmap_shape = {4, (std::size_t)(_ncells_max + 2), ndofs_per_cell};
+    }
+    else
+    {
+      _ddofmap.resize(_ncells_max * _ndof_flux_nz * 4, 0);
+      _dofmap_shape = {4, (std::size_t)_ncells_max, (std::size_t)_ndof_flux_nz};
+    }
+  }
+
+  void flux_dofmap_cell(const int a, mdspan_t<std::int32_t, 3> dofmap)
+  {
+    // Cell ID
+    const std::int32_t cell = _cells[a];
+
+    // Facet IDs
+    const std::int32_t fg_Eam1 = _fcts[a - 1], fg_Ea = _fcts[a];
+
+    std::int8_t fl_Eam1, fl_Ea;
+    std::tie(fl_Eam1, fl_Ea) = fctid_local(a);
+
+    // Set DOFmap
+    const std::int32_t gdof = cell * _ndof_flux;
+    std::int32_t ldof, pdof, pdof_Eam1, pdof_Ea, offs;
+    const bool internal_patch = is_internal();
+
+    if constexpr (id_flux_order == 1)
+    {
+      /* Facet DOFs */
+      // Cell-local
+      dofmap(0, a, 0) = fl_Eam1;
+      dofmap(0, a, 1) = fl_Ea;
+
+      // Global
+      const std::int32_t gdof = cell * _ndof_flux;
+
+      dofmap(1, a, 0) = gdof + fl_Eam1;
+      dofmap(1, a, 1) = gdof + fl_Ea;
+
+      // Patch-local
+      dofmap(2, a, 0) = 0;
+      dofmap(2, a, 1) = 0;
+    }
+    else
+    {
+      /* Facet DOFs */
+      offs = _ndof_flux_fct;
+
+      if (internal_patch && (a == _ncells))
+      {
+        pdof_Eam1 = (a - 1) * (_ndof_flux_fct - 1);
+        pdof_Ea = 0;
+      }
+      else
+      {
+        pdof_Eam1 = (a - 1) * (_ndof_flux_fct - 1);
+        pdof_Ea = pdof_Eam1 + _ndof_flux_fct - 1;
+      }
+
+      for (std::size_t ii = 0; ii < _ndof_flux_fct; ++ii)
+      {
+        // Cell-local
+        std::int32_t ldof_Eam1 = fl_Eam1 * _ndof_flux_fct + ii;
+        std::int32_t ldof_Ea = fl_Ea * _ndof_flux_fct + ii;
+
+        dofmap(0, a, ii) = ldof_Eam1;
+        dofmap(0, a, offs) = ldof_Ea;
+
+        // Global
+        dofmap(1, a, ii) = gdof + ldof_Eam1;
+        dofmap(1, a, offs) = gdof + ldof_Ea;
+
+        // Patch-local
+        if (ii == 0)
+        {
+          dofmap(2, a, ii) = 0;
+          dofmap(2, a, offs) = 0;
+        }
+        else
+        {
+          dofmap(2, a, ii) = pdof_Eam1 + ii;
+          dofmap(2, a, offs) = pdof_Ea + ii;
+        }
+
+        // Update offset
+        offs += 1;
+      }
+
+      /* Cell DOFs: Additional */
+      if constexpr (id_flux_order > 2)
+      {
+        offs = 2 * _ndof_flux_fct;
+        ldof = _fct_per_cell * _ndof_flux_fct + _ndof_flux_div_cell;
+        pdof
+            = _nfcts * (_ndof_flux_fct - 1) + 1 + (a - 1) * _ndof_flux_add_cell;
+
+        for (std::size_t ii = 0; ii < _ndof_flux_add_cell; ++ii)
+        {
+          // Cell-local
+          dofmap(0, a, offs) = ldof;
+
+          // Global
+          dofmap(1, a, offs) = gdof + ldof;
+
+          // Patch-local
+          dofmap(2, a, offs) = pdof + ii;
+
+          // Update offset/ local DOF
+          offs += 1;
+          ldof += ii;
+        }
+      }
+
+      /* Cell DOFs: Divergence */
+      offs = 2 * _ndof_flux_fct + _ndof_flux_add_cell;
+      ldof = _fct_per_cell * _ndof_flux_fct;
+
+      for (std::size_t ii = 0; ii < _ndof_flux_div_cell; ++ii)
+      {
+        // Cell-local
+        dofmap(0, a, offs) = ldof;
+
+        // Global
+        dofmap(1, a, offs) = gdof + ldof;
+
+        // Update offset/ local DOF
+        offs += 1;
+        ldof += 1;
+      }
+    }
+
+    /* DOFs constraint */
+    if constexpr (constr_minms)
+    {
+      if (is_internal())
+      {
+        fctdofs_contraint_space(a, dofmap);
+      }
+      else
+      {
+        if (a == 1)
+        {
+          fctdofs_contraint_space(0, a, dofmap);
+          fctdofs_contraint_space(1, dofmap);
+        }
+        if (a == _ncells)
+        {
+          fctdofs_contraint_space(a, a, dofmap);
+        }
+        else
+        {
+          fctdofs_contraint_space(a, dofmap);
+        }
+      }
+    }
+  }
+
+  void fctdofs_contraint_space(const int a, mdspan_t<std::int32_t, 3> dofmap)
+  {
+    // Nodes on facet ii
+    std::span<const std::int32_t> nodes_on_fct = _fct_to_node->links(_fcts[a]);
+
+    // Information of cells adjacent to facet a
+    const int pcell_1 = a;
+    const int pcell_2 = (a == _ncells) ? 1 : a + 1;
+
+    const std::int32_t cell_1 = _cells[pcell_1];
+    const std::int32_t cell_2 = _cells[pcell_2];
+
+    // Determine the facet DOFs
+    const int offs = 2 * _ndof_flux_fct + _ndof_flux_add_cell;
+    for (std::int32_t node : nodes_on_fct)
+    {
+      if (node == _nodei)
+      {
+        // Cell-local DOF
+        dofmap(0, pcell_1, offs) = _inodes_local[pcell_1];
+        dofmap(0, pcell_2, offs) = _inodes_local[pcell_2];
+
+        // Patch-local DOF
+        dofmap(2, pcell_1, offs) = _ndof_min_flux;
+        dofmap(2, pcell_2, offs) = _ndof_min_flux;
+      }
+      else
+      {
+        int offs_1 = offs + 1;
+        int offs_2 = offs + 2;
+        int pdof = _ndof_min_flux + a;
+
+        // Cell-local DOF
+        dofmap(0, pcell_1, offs_1) = (std::int32_t)node_local(cell_1, node);
+        dofmap(0, pcell_2, offs_2) = (std::int32_t)node_local(cell_2, node);
+
+        // Patch-local DOF
+        dofmap(2, pcell_1, offs_1) = pdof;
+        dofmap(2, pcell_2, offs_2) = pdof;
+      }
+    }
+  }
+
+  void fctdofs_contraint_space(const int pfct, const int pcell,
+                               mdspan_t<std::int32_t, 3> dofmap)
+  {
+    // Nodes on facet ii
+    std::span<const std::int32_t> nodes_on_fct
+        = _fct_to_node->links(_fcts[pfct]);
+    const std::int32_t cell = _cells[pcell];
+
+    /* Set DOFmap */
+    // Offset and patch-local DOF
+    int offs, pdof;
+
+    if (pfct == 0)
+    {
+      offs = 2 * _ndof_flux_fct + _ndof_flux_add_cell + 2;
+      pdof = _ndof_min_flux + _nfcts - 1;
+    }
+    else
+    {
+      offs = 2 * _ndof_flux_fct + _ndof_flux_add_cell + 1;
+      pdof = _ndof_min_flux + _nfcts;
+    }
+
+    // Get additional node on facet
+    std::int32_t node
+        = (nodes_on_fct[0] == _nodei) ? nodes_on_fct[1] : nodes_on_fct[0];
+
+    // Set cell-local DOF
+    dofmap(0, pcell, offs) = (std::int32_t)node_local(cell, node);
+
+    // Patch-local DOF
+    dofmap(2, pcell, offs) = pdof;
+  }
+
+  void set_assembly_informations(const std::vector<bool>& facet_orientation,
+                                 std::span<const double> storage_detJ)
+  {
+    for (std::size_t id_a = 0; id_a < this->_ncells; ++id_a)
+    {
+      // Local facet IDs
+
+      // Prefactors
+
+      // Set informations
+    }
+  }
+
+  /* Overload functions from base-class */
+  void create_subdofmap(const int node_i)
+  {
+    // Initialize patch
+    initialize_patch(node_i);
+
+    // Set DOF counters for (constraint) minimisation problem
+    _ndof_min_flux
+        = 1 + _degree_elmt_fluxhdiv * _nfcts + _ndof_flux_add_cell * _ncells;
+
+    if constexpr (constr_minms)
+    {
+      _ndof_min_cons = _nfcts + 1;
+      _ndof_min = _ndof_min_flux + _ndof_min_cons;
+    }
+    else
+    {
+      _ndof_min_cons = 0;
+      _ndof_min = _ndof_min_flux;
+    }
+
+    // Adjust pattern for mdspan of DOFmap
+    _dofmap_shape[1] = _ncells + 2;
+
+    // Create mdspan of DOFmap
+    mdspan_t<std::int32_t, 3> dofmap(_ddofmap.data(), _dofmap_shape);
+
+    // Initialise DOFmap on each cell of the patch
+    for (std::size_t a = 1; a < _ncells + 1; ++a)
+    {
+      flux_dofmap_cell(a, dofmap);
+    }
+
+    // Complete DOFmap
+    if (is_internal())
+    {
+      // Set DOFmap on cell 0
+      for (std::size_t ii = 0; ii < dofmap.extent(2); ++ii)
+      {
+        // DOFmap on cell 0 (=ncells)
+        dofmap(0, 0, ii) = dofmap(0, _ncells, ii);
+        dofmap(1, 0, ii) = dofmap(1, _ncells, ii);
+        dofmap(2, 0, ii) = dofmap(2, _ncells, ii);
+
+        // DOFmap on cell ncells+1 (=1)
+        dofmap(0, _ncells + 1, ii) = dofmap(0, 1, ii);
+        dofmap(1, _ncells + 1, ii) = dofmap(1, 1, ii);
+        dofmap(2, _ncells + 1, ii) = dofmap(2, 1, ii);
+      }
+    }
+  }
+
+  /* Setter functions */
+
+  /* Getter functions (Geometry) */
+
+  /// Get global node-ids on facet
+  /// @param fct_i Patch-local facet-id
+  /// @return List of nodes on facets
+  std::span<const std::int32_t> nodes_on_fct(int fct_i)
+  {
+    return _fct_to_node->links(_fcts[fct_i]);
+  }
+
+  /// Return local facet id of E_a on T_a
+  ///
+  /// T_a has only two facets, that are relevant for current patch.
+  /// Following the convention, T_a is adjacent to E_am1 and E_a!
+  ///
+  /// @param fct_i  Patch-local id of facet E_a (a>=0)
+  ///               (inner patches: a=0 <-> a=nfcts)
+  /// @param cell_i Patch-local id of cell T_a (a>0)
+  /// @return Local facte id
+  std::int8_t fctid_local(int fct_i, int cell_i)
+  {
+    int offst;
+
+    if (_type[0] == PatchType::internal)
+    {
+      if (fct_i == 0 || fct_i == _ncells)
+      {
+        // Allows labeling cell 1 as cell _ncells + 1
+        // and labeling cell _ncells as cell 0
+        if (cell_i == 1 || cell_i == _ncells + 1)
+        {
+          offst = 1;
+        }
+        else if (cell_i == _ncells || cell_i == 0)
+        {
+          offst = 0;
+        }
+        else
+        {
+          throw std::runtime_error("Cell not adjacent to facet");
+        }
+      }
+      else
+      {
+        if (cell_i == fct_i)
+        {
+          offst = 0;
+        }
+        else if (cell_i == fct_i + 1)
+        {
+          offst = 1;
+        }
+        else
+        {
+          throw std::runtime_error("Cell not adjacent to facet");
+        }
+      }
+    }
+    else
+    {
+      if (cell_i < 1)
+      {
+        throw std::runtime_error("No such cell on patch!");
+      }
+
+      if (fct_i == 0)
+      {
+        if (cell_i == 1)
+        {
+          offst = 0;
+        }
+        else
+        {
+          throw std::runtime_error("Cell not adjacent to facet");
+        }
+      }
+      else
+      {
+        if (cell_i == fct_i)
+        {
+          offst = 0;
+        }
+        else if (cell_i == fct_i + 1)
+        {
+          offst = 1;
+        }
+        else
+        {
+          throw std::runtime_error("Cell not adjacent to facet");
+        }
+      }
+    }
+
+    return _fcts_local[2 * fct_i + offst];
+  }
+
+  /// Return local facet ids of E_am1 and E_a on cell T_a
+  /// @param cell_i Patch-local id of cell T_a (a>0)
+  /// @return Local facet ids of facets E_am1 and E_a
+  std::pair<std::int8_t, std::int8_t> fctid_local(int cell_i)
+  {
+    assert(cell_i > 0);
+
+    const int offs = 2 * cell_i;
+    return {_fcts_local[offs - 1], _fcts_local[offs]};
+  }
+
+  /* Getter functions (DOFmap) */
+  mdspan_t<const int32_t, 3> data_dofmap_minmisation() const
+  {
+    return mdspan_t<const int32_t, 3>(_ddofmap.data(), _dofmap_shape);
+  }
+
+protected:
+  /* Variables */
+  // Element degree of fluxes (degree of RT elements starts with 0!)
+  const int _degree_elmt_fluxhdiv;
+
+  // The function space
+  std::shared_ptr<const fem::FunctionSpace> _function_space_fluxhdiv;
+
+  // Number of DOFs on sub-elements (element definition)
+  int _ndof_flux_fct, _ndof_flux_div_cell, _ndof_flux_add_cell, _ndof_flux_cell,
+      _ndof_flux, _ndof_flux_nz;
+  int _ndof_min, _ndof_min_flux, _ndof_min_cons;
+};
+
 template <typename T, int id_flux_order = 3>
 class PatchFluxCstm : public Patch
 {
