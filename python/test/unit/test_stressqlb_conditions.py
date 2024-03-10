eb3f1994e379ffb33ca0f9e0e7421dc2206c3e79
maximilian-brodbeck <maximilian.brodbeck@isd.uni-stuttgart.de>
2024-03-10 08:13:27 +0100

    Add basic unit test for stress equilibration
    * Test divergence condition
    * Test jump condition
    
    --> Fullfilled even with wekly sym. stress exept for case with
        RT_2
    
    Open issue:
    * Add check for weak symmetry condition

---
+++ python/test/unit/test_stressqlb_conditions.py
@@ -0,0 +1,148 @@
+# --- Import ---
+import pytest
+
+import dolfinx.mesh as dmesh
+import dolfinx.fem as dfem
+import ufl
+
+from dolfinx_eqlb.eqlb import FluxEqlbSE, FluxEqlbEV
+import dolfinx_eqlb.eqlb.check_eqlb_conditions as eqlb_checker
+
+from utils import (
+    create_unitsquare_builtin,
+    create_unitsquare_gmsh,
+)
+
+from testcase_general import set_arbitrary_rhs, set_arbitrary_bcs
+from testcase_elasticity import solve_primal_problem, equilibrate_stresses
+
+""" 
+Check if equilibrated flux 
+    a.) fulfills the divergence condition div(sigma_eqlb) = f 
+    b.) is in the H(div) space
+    c.) fulfills the flux boundary conditions strongly
+"""
+
+@pytest.mark.parametrize("mesh_type", ["builtin"])
+@pytest.mark.parametrize("degree", [1,2,3,4])
+@pytest.mark.parametrize("bc_type", ["pure_dirichlet"])
+def test_equilibration_conditions(mesh_type, degree, bc_type):
+    # Create mesh
+    if mesh_type == "builtin":
+        geometry = create_unitsquare_builtin(
+            2, dmesh.CellType.triangle, dmesh.DiagonalType.crossed
+        )
+    elif mesh_type == "gmsh":
+        raise NotImplementedError("GMSH mesh not implemented yet")
+    else:
+        raise ValueError("Unknown mesh type")
+
+    # Initialise loop over degree of boundary flux
+    if bc_type != "neumann_inhom":
+        degree_max_rhs = 1
+    else:
+        degree_max_rhs = degree
+
+    # Perform tests
+    for degree_bc in range(0, degree_max_rhs):
+        for degree_prime in range(1, degree + 1):
+            for degree_rhs in range(0, degree):
+                # Set function space
+                V_prime = dfem.VectorFunctionSpace(geometry.mesh, ("P", degree_prime))
+
+                # Determine degree of projected quantities (primal flux, RHS)
+                degree_proj = max(degree_prime - 1, degree_rhs)
+
+                # Set RHS
+                rhs, rhs_projected = set_arbitrary_rhs(
+                    geometry.mesh,
+                    degree_rhs,
+                    degree_projection=degree_proj,
+                    vector_valued=True,
+                )
+
+                # Set boundary conditions
+                (
+                    boundary_id_dirichlet,
+                    boundary_id_neumann,
+                    dirichlet_functions,
+                    neumann_functions,
+                    neumann_projection,
+                ) = set_arbitrary_bcs(bc_type, V_prime, degree, degree_bc)
+
+                # Solve primal problem
+                u_prime, sigma_projected = solve_primal_problem(
+                    V_prime,
+                    geometry,
+                    boundary_id_neumann,
+                    boundary_id_dirichlet,
+                    rhs,
+                    neumann_functions,
+                    dirichlet_functions,
+                    degree_projection=degree_proj,
+                )
+
+                # Solve equilibration
+                rhs_projected_row = []
+
+                V_aux = dfem.FunctionSpace(
+                    geometry.mesh,
+                    ("DG", rhs_projected.function_space.element.basix_element.degree),
+                )
+
+                for i in range(geometry.mesh.geometry.dim):
+                    rhs_projected_row.append(dfem.Function(V_aux))
+
+                    # Set values from mixed space
+                    rhs_projected_row[-1].x.array[:] = (
+                        rhs_projected.sub(i).collapse().x.array[:]
+                    )
+
+                sigma_eq, boundary_dofvalues = equilibrate_stresses(
+                    FluxEqlbSE,
+                    degree,
+                    geometry,
+                    sigma_projected,
+                    rhs_projected_row,
+                    [boundary_id_neumann, boundary_id_neumann],
+                    [boundary_id_dirichlet, boundary_id_dirichlet],
+                    [neumann_functions, neumann_functions],
+                    [neumann_projection, neumann_projection],
+                )
+
+                # # --- Check boundary conditions ---
+                # if bc_type != "pure_dirichlet":
+                #     raise NotImplementedError(
+                #         "Neumann boundary conditions not implemented yet"
+                #     )
+
+                # --- Check divergence condition ---
+                stress_eq = ufl.as_matrix(
+                    [[sigma_eq[0][0], sigma_eq[0][1]], [sigma_eq[1][0], sigma_eq[1][1]]]
+                )
+                stress_projected = ufl.as_matrix(
+                    [
+                        [sigma_projected[0][0], sigma_projected[0][1]],
+                        [sigma_projected[1][0], sigma_projected[1][1]],
+                    ]
+                )
+
+                eqlb_checker.check_divergence_condition(
+                    stress_eq,
+                    stress_projected,
+                    rhs_projected,
+                    mesh=geometry.mesh,
+                    degree=degree,
+                    flux_is_dg=True,
+                )
+
+                # --- Check jump condition (only required for semi-explicit equilibrator)
+                for i in range(geometry.mesh.geometry.dim):
+                    eqlb_checker.check_jump_condition(sigma_eq[i], sigma_projected[i])
+
+
+if __name__ == "__main__":
+    import sys
+
+    pytest.main(sys.argv)
+    # test_equilibration_conditions("builtin", 3, "pure_dirichlet")
---
+++ python/test/unit/testcase_elasticity.py
@@ -0,0 +1,225 @@
+# --- Includes ---
+import numpy as np
+from petsc4py import PETSc
+from typing import Any, Callable, List
+
+import dolfinx
+import dolfinx.fem as dfem
+import ufl
+
+from dolfinx_eqlb.cpp import weak_symmetry_stress
+from dolfinx_eqlb.lsolver import local_projection
+from dolfinx_eqlb.eqlb import fluxbc
+
+from utils import Geometry, interpolate_ufl_to_function
+
+"""
+Setup variable test-cases for linear elasticity
+
+Supported variants:
+    - manufactured solution based on 
+      u_ext = [sin(2*pi * x) * cos(2*pi * y), -cos(2*pi * x) * sin(2*pi * y)]
+    - arbitrary right-hand side
+"""
+
+
+# --- Definition of manufactured solution
+def exact_solution(x):
+    """Exact solution
+    u_ext = [sin(2*pi * x) * cos(2*pi * y), -cos(2*pi * x) * sin(2*pi * y)]
+
+    Args:
+        x (ufl.SpatialCoordinate): The position x
+    Returns:
+        The exact function as ufl-expression
+    """
+    return ufl.as_vector(
+        [
+            ufl.sin(2 * ufl.pi * x[0]) * ufl.cos(2 * ufl.pi * x[1]),
+            -ufl.cos(2 * ufl.pi * x[0]) * ufl.cos(2 * ufl.pi * x[1]),
+        ]
+    )
+
+
+def exact_stress_linelast(x, gdim=2):
+    """Exact flux
+    sigma_ext = 2 * epsilon(u_ext) + div(u_ext) * I
+
+    Args:
+        x (ufl.SpatialCoordinate): The position x
+        gdim (int): The spatial dimension
+    Returns:
+        The exact stress at positions x as ufl expression
+    """
+
+    # The exact displacement
+    u_ext = exact_solution(x)
+
+    return 2 * ufl.sym(ufl.grad(u_ext)) + ufl.div(u_ext) * ufl.Identity(gdim)
+
+
+# --- Solution routines
+def solve_primal_problem(
+    V_prime: dfem.FunctionSpace,
+    geometry: Geometry,
+    bc_id_neumann: List[int],
+    bc_id_dirichlet: List[int],
+    ufl_rhs: Any,
+    ufl_neumann: List[Any],
+    u_dirichlet: List[dfem.Function],
+    degree_projection: int = -1,
+):
+    """Solves a poisson problem based on lagrangian finite elements
+
+    Args:
+        V_prime (dolfinx.FunctionSpace):      The function space of the primal problem
+        geometry (Geometry):                  The geometry of the domain
+        bc_id_neumann (List[int]):            List of boundary ids for neumann BCs
+        bc_id_dirichlet (List[int]):          List of boundary ids for dirichlet BCs
+        ufl_rhs (ufl):                        The right-hand side of the primal problem
+        ufl_neumann (List[ufl]):              List of neumann boundary conditions
+        u_dirichlet (List[dolfinx.Function]): List of dirichlet boundary conditions
+        degree_projection (int):              Degree of projected flux
+    """
+    # Check input
+    if len(bc_id_dirichlet) == 0:
+        raise ValueError("Pure neumann problem not supported!")
+
+    if len(list(set(bc_id_dirichlet) & set(bc_id_neumann))) > 0:
+        raise ValueError("Overlapping boundary data!")
+
+    # The spatial dimension
+    gdim = geometry.mesh.topology.dim
+
+    # Set variational form
+    u = ufl.TrialFunction(V_prime)
+    v = ufl.TestFunction(V_prime)
+
+    sigma = 2 * ufl.sym(ufl.grad(u)) + ufl.div(u) * ufl.Identity(gdim)
+
+    a_prime = ufl.inner(sigma, ufl.grad(v)) * ufl.dx
+    l_prime = ufl.inner(ufl_rhs, v) * ufl.dx
+
+    # Set dirichlet boundary conditions
+    bcs_esnt = []
+
+    for i, id in enumerate(bc_id_dirichlet):
+        fcts = geometry.facet_function.indices[geometry.facet_function.values == id]
+        dofs = dfem.locate_dofs_topological(V_prime, 1, fcts)
+        bcs_esnt.append(dfem.dirichletbc(u_dirichlet[i], dofs))
+
+    # Set neumann boundary conditions
+    for i, id in enumerate(bc_id_neumann):
+        l_prime -= ufl.inner(ufl_neumann[i], v) * geometry.ds(id)
+
+    # Solve problem
+    solveoptions = {
+        "ksp_type": "preonly",
+        "pc_type": "lu",
+        "ksp_rtol": 1e-12,
+        "ksp_atol": 1e-12,
+    }
+    problem_prime = dfem.petsc.LinearProblem(
+        a_prime, l_prime, bcs_esnt, petsc_options=solveoptions
+    )
+    u_prime = problem_prime.solve()
+
+    # Project flux
+    if degree_projection < 0:
+        degree_projection = V_prime.element.basix_element.degree - 1
+
+    sigma_h = 2 * ufl.sym(ufl.grad(u_prime)) + ufl.div(u_prime) * ufl.Identity(gdim)
+
+    V_flux = dfem.VectorFunctionSpace(geometry.mesh, ("DG", degree_projection))
+    sig_proj = local_projection(
+        V_flux,
+        [
+            -ufl.as_vector([sigma_h[0, 0], sigma_h[0, 1]]),
+            -ufl.as_vector([sigma_h[1, 0], sigma_h[1, 1]]),
+        ],
+    )
+
+    return u_prime, sig_proj
+
+
+def equilibrate_stresses(
+    Equilibrator: Any,
+    degree_flux: int,
+    geometry: Geometry,
+    sig_proj: List[dfem.Function],
+    rhs_proj: List[dfem.Function],
+    bc_id_neumann: List[List[int]],
+    bc_id_dirichlet: List[List[int]],
+    flux_neumann: List[Any],
+    neumann_projection: List[bool],
+):
+    """Equilibrates the fluxes of the primal problem
+
+    Args:
+        Equilibrator (equilibration.FluxEquilibrator): The equilibrator object
+        degree_flux (int):                             Degree of flux space
+        geometry (Geometry):                           The geometry of the domain
+        sig_proj (List[dfem.Function]):                List of projected fluxes
+        rhs_proj (List[dfem.Function]):                List of projected right-hand sides
+        bc_id_neumann (List[List[int]]):               List of boundary ids for neumann BCs
+        bc_id_dirichlet (List[List[int]]):             List of boundary ids for dirichlet BCs
+        flux_neumann (List[List[ufl]]):                List of neumann boundary conditions
+        neumann_projection (List[List[bool]]):         List of booleans indicating wether the
+                                                       neumann BCs require projection
+
+    Returns:
+        List[dfem.Function]: List of equilibrated fluxes
+        List[dfem.Function]: List boundary-functions
+                             (functions, containing the correct boundary values)
+    """
+    # Extract facet markers
+    fct_values = geometry.facet_function.values
+
+    # Set equilibrator
+    equilibrator = Equilibrator(degree_flux, geometry.mesh, rhs_proj, sig_proj)
+
+    # Mark dirichlet facets of primal problem
+    fct_bcesnt_primal = []
+
+    for list_bc_id in bc_id_dirichlet:
+        if len(list_bc_id) > 0:
+            fct_numpy = np.array([], dtype=np.int32)
+
+            for id_esnt in list_bc_id:
+                list_fcts = geometry.facet_function.indices[fct_values == id_esnt]
+                fct_numpy = np.concatenate((fct_numpy, list_fcts))
+
+            fct_bcesnt_primal.append(fct_numpy)
+
+    # Set Neumann conditions on flux space
+    bc_esnt_flux = []
+
+    for i in range(0, len(bc_id_neumann)):
+        if len(bc_id_neumann[i]) > 0:
+            list_flux_bc = []
+
+            for j, id_flux in enumerate(bc_id_neumann[i]):
+                list_fcts = geometry.facet_function.indices[fct_values == id_flux]
+                list_flux_bc.append(
+                    fluxbc(
+                        flux_neumann[i][j],
+                        list_fcts,
+                        equilibrator.V_flux,
+                        neumann_projection[i][j],
+                    )
+                )
+
+            bc_esnt_flux.append(list_flux_bc)
+        else:
+            bc_esnt_flux.append([])
+
+    # Set equilibrator
+    equilibrator.set_boundary_conditions(fct_bcesnt_primal, bc_esnt_flux)
+
+    # Step 1: Equilibrate stress tensor without symmetry
+    equilibrator.equilibrate_fluxes()
+
+    # Step 2: Enforce symmetry in a weak sense
+    # weak_symmetry_stress(equilibrator.list_flux_cpp, equilibrator.boundary_data)
+
+    return equilibrator.list_flux, equilibrator.list_bfunctions
