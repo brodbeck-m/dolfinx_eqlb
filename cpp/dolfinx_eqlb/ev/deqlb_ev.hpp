9d6604e516745de21bcea6d745180ddc0b79e7e6
maximilian-brodbeck <maximilian.brodbeck@isd.uni-stuttgart.de>
2024-11-22 17:22:15 +0100

    Reworks project structure

---
+++ cpp/dolfinx_eqlb/ev/deqlb_ev.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+/// @brief Flux equilibration functionalities of the Ern and Vohralík type.
+///
+/// Classes and algorithms for the equilibration of fluxes and stresses based on
+/// the approach of Ern and Vohralík [1] and a weakly symmetric equilibration of
+/// stresses following [2].
+///
+/// [1] Ern, A. and Vohralík, M.: https://doi.org/10.1137/130950100, 2015
+/// [2] Bertrand, F. et al.: https://doi.org/10.1002/num.22741, 2021
+
+namespace dolfinx_eqlb::ev
+{
+}
+
+// dolfinx_eqlb ev interface
---
+++ cpp/dolfinx_eqlb/se/deqlb_se.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+/// @brief Flux equilibration functionalities based on a semi-explicit approach.
+/// Classes and algorithms for the equilibration of fluxes [1, 2] and
+/// stresses [3] based on the semi-explicit approach .
+///
+/// [1] Cai, Z. and Zhang, S.: https://doi.org/10.1137/100803857, 2012
+/// [2] Bertrand, F. et al.: https://doi.org/10.1007/s00211-023-01366-8, 2023
+/// [3] Bertrand, F. et al.: https://doi.org/10.1002/num.22741, 2021
+
+namespace dolfinx_eqlb::ev
+{
+}
+
+// dolfinx_eqlb se interface
